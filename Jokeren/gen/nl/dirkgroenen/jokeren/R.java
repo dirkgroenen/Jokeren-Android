4409be2a48995668908abf2e35266400a71f9b25
Dirk Groenen <dirk_groenen@live.nl>
2013-01-08 13:37:30 +0100

    Stirng and images added

--- Jokeren/gen/nl/dirkgroenen/jokeren/R.java
+++ Jokeren/gen/nl/dirkgroenen/jokeren/R.java
@@ -11,10 +11,94 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bred=0x7f020000;
+        public static final int c10=0x7f020001;
+        public static final int c2=0x7f020002;
+        public static final int c3=0x7f020003;
+        public static final int c4=0x7f020004;
+        public static final int c5=0x7f020005;
+        public static final int c6=0x7f020006;
+        public static final int c7=0x7f020007;
+        public static final int c8=0x7f020008;
+        public static final int c9=0x7f020009;
+        public static final int ca=0x7f02000a;
+        public static final int cj=0x7f02000b;
+        public static final int ck=0x7f02000c;
+        public static final int cq=0x7f02000d;
+        public static final int d10=0x7f02000e;
+        public static final int d2=0x7f02000f;
+        public static final int d3=0x7f020010;
+        public static final int d4=0x7f020011;
+        public static final int d5=0x7f020012;
+        public static final int d6=0x7f020013;
+        public static final int d7=0x7f020014;
+        public static final int d8=0x7f020015;
+        public static final int d9=0x7f020016;
+        public static final int da=0x7f020017;
+        public static final int deck=0x7f020018;
+        public static final int deckred=0x7f020019;
+        public static final int dj=0x7f02001a;
+        public static final int dk=0x7f02001b;
+        public static final int dq=0x7f02001c;
+        public static final int h10=0x7f02001d;
+        public static final int h2=0x7f02001e;
+        public static final int h3=0x7f02001f;
+        public static final int h4=0x7f020020;
+        public static final int h5=0x7f020021;
+        public static final int h6=0x7f020022;
+        public static final int h7=0x7f020023;
+        public static final int h8=0x7f020024;
+        public static final int h9=0x7f020025;
+        public static final int ha=0x7f020026;
+        public static final int hj=0x7f020027;
+        public static final int hk=0x7f020028;
+        public static final int hq=0x7f020029;
+        public static final int ic_launcher=0x7f02002a;
+        public static final int j=0x7f02002b;
+        public static final int kh=0x7f02002c;
+        public static final int qh=0x7f02002d;
+        public static final int s10=0x7f02002e;
+        public static final int s2=0x7f02002f;
+        public static final int s3=0x7f020030;
+        public static final int s4=0x7f020031;
+        public static final int s5=0x7f020032;
+        public static final int s6=0x7f020033;
+        public static final int s7=0x7f020034;
+        public static final int s8=0x7f020035;
+        public static final int s9=0x7f020036;
+        public static final int sa=0x7f020037;
+        public static final int sj=0x7f020038;
+        public static final int sk=0x7f020039;
+        public static final int sq=0x7f02003a;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int ivDeck=0x7f070003;
+        public static final int ivDeckOpen=0x7f070002;
+        public static final int ivcard0_1=0x7f070006;
+        public static final int ivcard0_2=0x7f070007;
+        public static final int ivcard0_3=0x7f070008;
+        public static final int ivcard0_4=0x7f070009;
+        public static final int ivcard0_5=0x7f07000a;
+        public static final int ivcard0_6=0x7f07000b;
+        public static final int ivcard0_7=0x7f07000c;
+        public static final int llCardDeck=0x7f070005;
+        public static final int llbottomcards=0x7f070004;
+        public static final int llplayground=0x7f07000d;
+        public static final int llplaygroundColumn1_1=0x7f07000f;
+        public static final int llplaygroundColumn1_2=0x7f070010;
+        public static final int llplaygroundColumn1_3=0x7f070011;
+        public static final int llplaygroundColumn1_4=0x7f070012;
+        public static final int llplaygroundColumn1_5=0x7f070013;
+        public static final int llplaygroundColumn2_1=0x7f070015;
+        public static final int llplaygroundColumn2_2=0x7f070016;
+        public static final int llplaygroundColumn2_3=0x7f070017;
+        public static final int llplaygroundColumn2_4=0x7f070018;
+        public static final int llplaygroundColumn2_5=0x7f070019;
+        public static final int llplaygroundRow1=0x7f07000e;
+        public static final int llplaygroundRow2=0x7f070014;
+        public static final int lltopplayers=0x7f070000;
+        public static final int menu_settings=0x7f07001a;
+        public static final int tvOpp1=0x7f070001;
     }
     public static final class layout {
         public static final int gamescreen=0x7f030000;
@@ -27,6 +111,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int opp1=0x7f040003;
     }
     public static final class style {
         /** 
