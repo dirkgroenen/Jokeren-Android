743455e66df10fd7e9eb73b072281e4e9e4953ed
Dirk Groenen <dirk_groenen@live.nl>
2013-01-08 16:56:49 +0100

    Game class

---
+++ Jokeren/src/nl/dirkgroenen/jokeren/Game.java
@@ -0,0 +1,15 @@
+package nl.dirkgroenen.jokeren;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Game extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.gamescreen);
+	}
+	
+}
