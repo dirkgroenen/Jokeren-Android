4c2fa9b0adbb159ea51870810e062c0480899cb1
Dirk Groenen <dirk_groenen@live.nl>
2013-01-08 13:11:52 +0100

    First git publish for Jokeren app

---
+++ Jokeren/src/nl/dirkgroenen/jokeren/MainScreen.java
@@ -0,0 +1,22 @@
+package nl.dirkgroenen.jokeren;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainscreen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mainscreen, menu);
+		return true;
+	}
+
+}
